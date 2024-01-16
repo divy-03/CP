4f77cf4193f0a4869bd2c75365b4f9a0fb7cccca
divy-03 <127951881+divy-03@users.noreply.github.com>
2024-01-16 18:36:18 +0530

    [1921A] Better solution

--- 19210.cpp
+++ 19210.cpp
@@ -3,28 +3,14 @@ using namespace std;
 
 typedef int long long ll;
 #define print(a) cout << a << "\n"
-#define printab(a, b) cout << a << " " << b << "\n"
-#define readV(vec) for (auto &e: vec) cin >> e;
-#define debug(vec) for (auto &e: vec) cout << e << " ";
-#define nl cout << "\n"
-#define YES cout << "YES\n"
-#define NO cout << "NO\n"
 
 void solve() {
-
     vector<pair<long long, long long>> pair(4);
     for (auto &e: pair) cin >> e.first >> e.second;
     
-    ll a = 0, b = 0;
-    for (int j = 0; j < 4; j++) {
-        for (int i = 0; i < 3; i++) {
-            a = max(a, abs(pair[i].second - pair[j].second));
-            b = max(b, abs(pair[i].first - pair[j].first));
-        }
-    }
-
-    print(a*b);
-    
+    sort(pair.begin(), pair.end());
+    ll a = pair[0].first - pair[3].first;
+    print(a*a);    
 }
 
 int main() {
