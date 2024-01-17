af4a33ec32b35757d2c9be04038fc77a65323437
divy-03 <127951881+divy-03@users.noreply.github.com>
2024-01-17 16:42:05 +0530

    [456A] weird question

--- 456A.cpp
+++ 456A.cpp
@@ -14,26 +14,19 @@ void solve() {
     ll n;
     cin >> n;
 
-    vector <pair <ll, ll>> vec(n);
-    for (auto &e: vec) cin >> e.first >> e.second;
-
-    sort(vec.begin(), vec.end());
-
-    for (int i = 0; i < n-1; i++) {
-        if (vec[i].second <= vec[i+1].second) {
-            print("Poor Alex");
-            return;
-        }
-    }  
-    print("Happy Alex");
-    // for (auto &e: vec) {
-    //     cout << e.first << " ";
-    // }
-    // nl;
-    // for (auto &e: vec) {
-    //     cout << e.second << " ";
-    // }
-
+    vector <pair <ll, ll>> laptops(n);
+    for (auto &e: laptops) cin >> e.first >> e.second;
+    
+    sort(laptops.begin(),laptops.end());
+ 
+    bool isHappy = false;
+     for(int i = 1; i < n; i++) {
+          if(laptops[i].second < laptops[i - 1].second){
+            isHappy = true;
+          }
+     }
+     if(isHappy) cout << "Happy Alex";
+     else cout << "Poor Alex";
 }
 
 int main() {
