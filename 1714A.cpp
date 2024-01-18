c5fb3a0b9238157a77ff06623897c7f8d99e0f12
divy-03 <127951881+divy-03@users.noreply.github.com>
2024-01-18 17:41:32 +0530

    [1714A] Everyone Loves to Sleep (doubt)

--- 1714A.cpp
+++ 1714A.cpp
@@ -64,10 +64,7 @@ void solve()
     }
     int t = 0;
     while (!mp[(H * 60 + M + t) % (24 * 60)]) t++;
-    // t %= 24 * 60;
-    // print(t);
     cout << t / 60 << " " << t % 60 << "\n";
-    // for (auto &e: mp) cout << e.second << " ";
 }
 
 int main()
