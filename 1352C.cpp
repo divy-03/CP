b77789e54e3ad6a9b86ccbc24d41b0d4322900f8
divy-03 <127951881+divy-03@users.noreply.github.com>
2024-01-22 19:15:53 +0530

    [1352C] Tricky Q but ez tutorial sol

--- 1352C.cpp
+++ 1352C.cpp
@@ -1,25 +1,24 @@
+/*
+    excl - grey
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
 typedef int long long ll;
-#define print(a) cout << a << "\n"
-#define printab(a, b) cout << a << " " << b << "\n"
-#define readV(vec) for (auto &e: vec) cin >> e;
-#define debug(vec) for (auto &e: vec) cout << e << " ";
-#define nl cout << "\n"
-#define YES cout << "YES\n"
-#define NO cout << "NO\n"
+#define print(a) cout << a << "\n";
+
+const int N = 100005;
 
 void solve() {
-    ll n;
-    cin >> n;
-
+    ll n, k;
+    cin >> n >> k;
     
+    ll need = (k-1) / (n-1);
+    print(k+need);
 }
 
 int main() {
-    ll t;
+    int t = 1;
     cin >> t;
-
     while (t--) solve();
 }
