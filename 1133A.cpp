07b68b5dc3a1dea63b96ef568f12c100ee7c1cd0
divy-03 <127951881+divy-03@users.noreply.github.com>
2024-02-05 15:02:36 +0530

    [1133A] hr*60+min remember!!

--- 1133A.cpp
+++ 1133A.cpp
@@ -5,31 +5,13 @@
 using namespace std;
 
 typedef int long long ll;
-#define print(a) cout << a << "\n";
-#define printab(a,b) cout << a << " " << b << "\n";
-#define readV(vec) for (auto &e: vec) cin >> e;
-#define debug(vec) for (auto &e: vec) {cout << e << " ";} cout << "\n";
-#define nl cout << "\n"
-#define YES cout << "YES" << "\n"
-#define NO cout << "NO" << "\n"
 
-const int N = 100005;
-
-void solve() {
-    string n, k;
-    cin >> n >> k;
-    
-    n.erase(2, 1);
-    k.erase(2, 1);
-    int a = stoi(n), b = stoi(k);
-    // print(b-a);
-    int ans;
-    if (b-a!=100) ans = a + (b-a)/2;
-    else ans = a+30;
-    // print(ans);
+int f(string s){
+    return stoi(s.substr(0,2))*60+stoi(s.substr(3,2));
 }
-
 int main() {
-    int t = 1;
-    while (t--) solve();
+    string s,t;
+    cin>>s>>t;
+    int m=(f(s)+f(t))/2;
+    printf("%02d:%02d",m/60,m%60);
 }
--- 1891B.cpp
+++ 1891B.cpp
@@ -1,22 +1,54 @@
-// Testcase check karne ke liye hardcode kar rha hu !
 #include <bits/stdc++.h>
 using namespace std;
 
 typedef int long long ll;
-#define print(a) cout << a << "\n"
+#define print(a) cout << a << "\n";
+#define printab(a,b) cout << a << " " << b << "\n";
+#define readV(vec) for (auto &e: vec) cin >> e;
+#define debug(vec) for (auto &e: vec) {cout << e << " ";} cout << "\n";
+#define nl cout << "\n"
+#define YES cout << "YES" << "\n"
+#define NO cout << "NO" << "\n"
+
+const int N = 100005;
 
 void solve() {
     ll n, k;
     cin >> n >> k;
+
+    vector<ll> a(n); readV(a);
+    vector<ll> b(k);
+    for (auto &e: b) {
+        int a;
+        cin >> a;
+        e = 1LL << a;
+    }
+
+    vector<ll> additions(64, 0);  // Precompute the values to be added for each bit position
+
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < 64; j++) {
+            if ((b[i] >> j) & 1) {
+                additions[j] += b[i] / 2;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 64; j++) {
+            if ((a[i] >> j) & 1) {
+                a[i] += additions[j];
+            }
+        }
+    }
+
+    debug(a);
 }
 
 int main() {
-    ll t;
+    int t = 1;
     cin >> t;
-
     while (t--) solve();
-    print("9 19 11 12 12");
-    print("7 10 14 38 58 6 3");
-    print("3 3 3 3 3");
-    print("1 3 7 11 19");
+    return 0;
 }
+            
