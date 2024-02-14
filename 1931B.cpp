2b679ae85525df9f11a7d32b4ec2693a94072e80
divy-03 <127951881+divy-03@users.noreply.github.com>
2024-02-14 23:13:24 +0530

    [451A] ez 900

--- 1931B.cpp
+++ 1931B.cpp
@@ -5,11 +5,7 @@
 using namespace std;
 
 typedef int long long ll;
-#define print(a) cout << a << "\n";
-#define printab(a,b) cout << a << " " << b << "\n";
 #define readV(vec) for (auto &e: vec) cin >> e;
-#define debug(vec) for (auto &e: vec) {cout << e << " ";} cout << "\n";
-#define nl cout << "\n"
 #define YES cout << "YES" << "\n"
 #define NO cout << "NO" << "\n"
 
@@ -23,14 +19,12 @@ void solve() {
     ll sum = accumulate(vec.begin(), vec.end(), 0);
 
     ll wc=0;
-    bool res = true;
     for (auto &e: vec) {
         if (e > sum/n) wc += e-sum/n;
         else wc = wc - (sum/n-e);
-        if (wc < 0) res=false;
+        if (wc < 0) {NO; return;}
     }
-    if (res) YES;
-    else NO;
+    YES;
 }
 
 int main() {
--- 451A.cpp
+++ 451A.cpp
@@ -1,25 +1,13 @@
+/*
+    excl - grey
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef int long long ll;
-#define print(a) cout << a << "\n"
-#define printab(a, b) cout << a << " " << b << "\n"
-#define readV(vec) for (auto &e: vec) cin >> e;
-#define debug(vec) for (auto &e: vec) cout << e << " ";
-#define nl cout << "\n"
-#define YES cout << "YES\n"
-#define NO cout << "NO\n"
-
-void solve() {
-    ll n;
-    cin >> n;
-
-    
-}
-
 int main() {
-    ll t;
-    cin >> t;
-
-    while (t--) solve();
+    int a, b;
+    cin >> a >> b;
+    
+    if (min(a, b)&1) cout << "Akshat";
+    else cout << "Malvika";
 }
