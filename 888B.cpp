471f4fec2977d829465d57948e4b25d95fab3ba6
divy-03 <127951881+divy-03@users.noreply.github.com>
2024-03-11 14:01:27 +0530

    [888B] ez 1000

--- 888B.cpp
+++ 888B.cpp
@@ -7,25 +7,21 @@ using namespace std;
 typedef int long long ll;
 #define fastio ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
 #define print(a) cout << a << "\n"
-#define printab(a,b) cout << a << " " << b << "\n"
-#define readV(vec) for (auto &e: vec) cin >> e;
-#define debug(vec) for (auto &e: vec) {cout << e << " ";} cout << "\n";
-#define nl cout << "\n"
-#define YES cout << "YES" << "\n"
-#define NO cout << "NO" << "\n"
-
-const int N = 100005;
 
 void solve() {
-    ll n;
-    cin >> n;
+    ll n; cin >> n;
+    string s; cin >> s;
     
-    
+    ll cL, cD, cU, cR;
+    cL = cD = cU = cR = INT_MAX;
+    cL = count(s.begin(), s.end(), 'L');
+    cD = count(s.begin(), s.end(), 'D');
+    cU = count(s.begin(), s.end(), 'U');
+    cR = count(s.begin(), s.end(), 'R');
+
+    print(2*min(cL, cR) + 2*min(cU, cD));
 }
 
 int main() {
-    fastio;
-    int t = 1;
-    cin >> t;
-    while (t--) solve();
+    fastio; solve();
 }
