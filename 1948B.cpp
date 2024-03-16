6c948f0da6b09d814e4a988beef48be4bf71d236
divy-03 <127951881+divy-03@users.noreply.github.com>
2024-03-16 22:39:07 +0530

    [1928B] check & debug solution again

--- 1948B.cpp
+++ 1948B.cpp
@@ -16,13 +16,23 @@ void solve() {
     
     vector<ll> vec(n); readV(vec);
     ll idx = 0;
+
+    // Get the last index of the element smaller than previous one
     for (int i=0; i<n-1; i++) if (vec[i]>vec[i+1]) idx = i+1;
+    
+    // Break all of the numbers before the 'last' index.
+    // It can be proved that we need to break
+    // all the numbers before 'last' to make 
+    // them less than 'last' index.
     vector<ll> a;
     for (int i=0; i<idx; i++) {
         if (vec[i]/10!=0) a.push_back(vec[i]/10);
         a.push_back(vec[i]%10);
     }
+    
+    // Insert elements after 'last' index.
     for (int i=idx; i<n; i++) a.push_back(vec[i]);
+
     if (is_sorted(a.begin(), a.end())) YES
     else NO
 }
