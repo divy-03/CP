8c05bd21f084f1f8486e4520f49c16773579c669
divy-03 <127951881+divy-03@users.noreply.github.com>
2025-07-18 19:36:55 +0530

    my solution

--- 2126C.cpp
+++ 2126C.cpp
@@ -1,43 +1,45 @@
+/*
+    excl - grey
+*/
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long 
-#define endl '\n'
-#define readn for(i=0;i<n;i++) cin >> a[i]
-#define pb push_back
 
+typedef int long long ll;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define print(a) cout << a << "\n"
+#define printab(a,b) cout << a << " " << b << "\n"
+#define readV(vec) for (auto &e: vec) cin >> e;
+#define debug(vec) for (auto &e: vec) {cout << e << " ";} cout << "\n";
+#define nl cout << "\n"
+#define YES cout << "YES" << "\n"
+#define NO cout << "NO" << "\n"
+
+const int N = 100005;
+
+void solve() {
+    ll n, k, x, t=0;
+    cin >> n >> k;
+    
+    vector<ll> vec(n); 
+    for (int i = 0; i<n; i++) {
+        cin >> vec[i];
+        if (i == k-1) x = vec[i];
+    }
+    
+    sort(vec.begin(), vec.end());
+
+    for (int i = 0; i<n-1; i++) {
+        if (vec[i]>=x) {
+            t += vec[i+1]-vec[i];
+            if (vec[i] < t) {NO; return;}
+        }
+    }
+    YES;
+} 
 
 int main() {
-    // your code goes here
-    int t=1;
-    cin>>t;
-    while (t--)
-    {
-        int n,i,k,f=0;cin>>n>>k;vector<int>a(n);readn;
-        k=k-1;
-        int num=a[k],index,num1;
-        sort(a.begin(),a.end());
-        if(a[n-1]==num)
-        {
-            cout<<"YES"<<endl;
-            continue;
-        }
-        for (i=0;i<n-1; i++)
-        {
-            if(a[i]==num and a[i+1]!=num)
-            {
-                index=i;
-                break;
-            }
-        }
-        num1=a[index];
-        for(int i=index;i<n-1;i++)
-        {
-            if((a[i+1]-a[i])>num1)
-            {
-                f=1;
-                break;
-            }
-        }
-        cout<<(f==1 ? "NO" : "YES")<<endl;
-    }
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
 }
